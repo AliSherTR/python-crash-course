d9960eb1cc9e5a335931a34486b8951fdf0892d6
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2023-11-09 22:16:11 +0500

    chapter 2

---
+++ Chapter 2/practice.py
@@ -0,0 +1,13 @@
+name = 'ali sher khan'
+
+print(name.title())
+
+# The lower() method is particularly useful for storing data. Many times
+# you won’t want to trust the capitalization that your users provide, so you’ll
+# convert strings to lowercase before storing them
+
+first_name = "Ali"
+middle_name = 'Sher'
+last_name = 'khan'
+
+print(first_name + " " + middle_name + "\t "  + last_name)
