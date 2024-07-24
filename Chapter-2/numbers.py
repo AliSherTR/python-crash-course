224f8682530da4a474601116b72c228b4bc98458
Ali Sher Khan <94902748+AliSherTR@users.noreply.github.com>
2024-07-24 19:51:07 +0500

    chapter 1 + 2

---
+++ Chapter-2/numbers.py
@@ -0,0 +1,22 @@
+import this
+# 2-8. Number Eight: Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8 Be sure to enclose your operations
+# in print statements to see the results.
+
+
+print(5 + 3)
+
+print(2*4)
+
+print(10-2)
+
+print(16/2)
+
+
+# 2-9. Favorite Number: Store your favorite number in a variable Then, using
+# that variable, create a message that reveals your favorite number Print that
+# message
+
+favourite_number = 21
+
+print("My favourite number is " + str(favourite_number))
